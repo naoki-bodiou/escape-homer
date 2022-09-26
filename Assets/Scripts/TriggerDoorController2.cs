bb313251a1c9d635d6e911d8ca44a72b445d8d4c
naoki-bodiou <naoki.bodiou@viacesi.fr>
2022-09-26 15:39:04 +0200

    Current door without key press

--- Assets/Scripts/TriggerDoorController2.cs
+++ Assets/Scripts/TriggerDoorController2.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class TriggerDoorController : MonoBehaviour
+public class TriggerDoorController2 : MonoBehaviour
 {
-
     public Animator myDoor = null;
 
     public bool openTrigger = false;
     public bool closeTrigger = false;
 
-    public string doorOpen = "DoorOpen";
-    public string doorClose = "DoorClose";
+    //public string doorOpen = "DoorOpen";
+    //public string doorClose = "DoorClose";
 
     private void OnTriggerEnter(Collider other)
     {
+        //if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        //{
+        //    if (openTrigger == false && closeTrigger == false)
+        //    {
+        //        myDoor.play("DoorOpen", 0, 0.0f);
+        //    }
+
+
+
+
         if (other.gameObject.tag == "Player")
         {
             if (openTrigger)
@@ -28,6 +38,6 @@ public class TriggerDoorController : MonoBehaviour
                 //gameObject.setActive(false);
             }
         }
-    }
 
+    }
 }
